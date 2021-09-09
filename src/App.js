1174fd4d93cc33ce5598cb6408d5d72fd235180b
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-09-09 09:05:28 -0400

    Pendiente + tipo de pendiente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 const pendiente=document.querySelector("#pendiente-input");
+const tipoPendiente=document.querySelector("#tipo-pendiente")
 const form=document.querySelector("#pendiente-form");
 
 form.addEventListener("submit", (event)=>{
-    alert("Pendiente Ingresado: "+pendiente.value);
+    event.preventDefault();
+    let resPend=TypePend(tipoPendiente.value);
+    alert("Pendiente Ingresado: "+pendiente.value+" [ "+resPend+" ]");
 });
+
+function TypePend(tipoPend){
+    let res="";
+    switch(tipoPend){
+        case 'Work':
+            res="Work";
+            break;
+        case 'Personal':
+            res="Personal";
+            break;
+    }
+    return res;
+}
